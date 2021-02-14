efe0d52bd098ee850bb28573d5fbcc950726ef6d
berk karakas <karakasberk@hotmail.com>
2021-02-14 20:33:47 +0300

    10 Feb

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -17,6 +18,18 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            //business codes
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult("Product Name should be min 2 character");
+
+            }
+            _productDal.Add(product);
+            return new Result(true,"Product added");
+        }
+
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
@@ -27,6 +40,11 @@ namespace Business.Concrete
             return _productDal.GetAll(p => p.CategoryId == id);
         }
 
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p=>productId == productId);
+        }
+
         public List<Product> GetByUnitPrice(decimal min, decimal max)
         {
             return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
@@ -35,6 +53,11 @@ namespace Business.Concrete
         public List<ProductDetailDto> GetProductDetails()
         {
             return _productDal.GetProductDetails();
+        }
+
+        IResult IProductService.Add(Product product)
+        {
+            
         }
     }
 }
