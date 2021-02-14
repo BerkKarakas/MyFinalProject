a3d727f542b7561efdaa047b6e6b225dc5dfb1f9
berk karakas <karakasberk@hotmail.com>
2021-02-14 13:18:52 +0300

    060221

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,11 +1,10 @@
-﻿using Entities.Abstract;
-using Entities.Concrete;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint
     // class:referans tip
@@ -19,5 +18,12 @@ namespace DataAccess.Abstract
         void Delete(T entity);
 
         
+    }
+}
+
+namespace Core
+{
+    public interface IDto
+    {
     }
 }
